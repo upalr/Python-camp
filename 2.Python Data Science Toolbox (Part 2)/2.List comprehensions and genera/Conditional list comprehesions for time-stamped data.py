0e808e8beecea586d47ebad6d3c8b92ac5219bce
uralr <shovon.forums@gmail.com>
2017-05-27 01:31:46 +0600

    use comprehension for creating list

---
+++ 2.Python Data Science Toolbox (Part 2)/2.List comprehensions and genera/Conditional list comprehesions for time-stamped data.py
@@ -0,0 +1,8 @@
+# Extract the created_at column from df: tweet_time
+tweet_time = df['created_at']
+
+# Extract the clock time: tweet_clock_time
+tweet_clock_time = [entry[11:19] for entry in tweet_time if entry[17:19] == '19']
+
+# Print the extracted times
+print(tweet_clock_time)
